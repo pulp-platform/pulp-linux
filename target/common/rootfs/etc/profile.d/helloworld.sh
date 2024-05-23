7851f16e5715b61e7c56c545f953c20fcf6f9655
bluew <bluewww@users.noreply.github.com>
2024-05-24 01:28:31 +0200

    target/common: Add basic rootfs

---
+++ target/common/rootfs/etc/profile.d/helloworld.sh
@@ -0,0 +1,1 @@
+printf "\n\e[4;37m%s\e[1m\e[38;5;208m%s\e[38;5;209m%s\e[38;5;210m%s\e[38;5;211m%s\e[38;5;212m%s\e[38;5;177m%s\e[38;5;141m%s\e[38;5;105m%s\e[m\n\n" "[PULP Platform] Welcome to Linux on " "C" "h" "e" "s" "h" "i" "r" "e"
